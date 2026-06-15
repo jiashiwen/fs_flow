846f8f16da21005ca2cdaf27c0fbe6756a0be420
jiashiwen <jiashiwen126@126.com>
2026-06-15 19:50:55 +0800

    update dependence

--- src/commons/calculate_etag.rs
+++ src/commons/calculate_etag.rs
@@ -1,35 +1,41 @@
 use anyhow::Result;
-use crypto::digest::Digest;
-use crypto::md5::Md5;
+use md5::{Digest, Md5};
 use std::fs::File;
 use std::io::prelude::*;
-use std::iter::repeat;
+
+fn hex_encode(bytes: &[u8]) -> String {
+    bytes.iter().map(|b| format!("{:02x}", b)).collect()
+}
 
 #[allow(dead_code)]
 fn calculate_etag_from_read(f: &mut dyn Read, chunk_size: usize) -> Result<String> {
-    let mut md5 = Md5::new();
-    let mut concat_md5 = Md5::new();
+    let mut concat_hasher = Md5::new();
     let mut input_buffer = vec![0u8; chunk_size];
     let mut chunk_count = 0;
-    let mut current_md5: Vec<u8> = repeat(0).take((md5.output_bits() + 7) / 8).collect();
+    let mut first_hash = [0u8; 16];
 
-    let md5_result = loop {
+    loop {
         let amount_read = f.read(&mut input_buffer)?;
-        if amount_read > 0 {
-            md5.reset();
-            md5.input(&input_buffer[0..amount_read]);
-            chunk_count += 1;
-            md5.result(&mut current_md5);
-            concat_md5.input(&current_md5);
-        } else {
-            if chunk_count > 1 {
-                break format!("{}-{}", concat_md5.result_str(), chunk_count);
-            } else {
-                break md5.result_str();
-            }
+        if amount_read == 0 {
+            break;
         }
-    };
-    Ok(md5_result)
+
+        let hash = Md5::digest(&input_buffer[0..amount_read]);
+        concat_hasher.update(&hash);
+
+        if chunk_count == 0 {
+            first_hash.copy_from_slice(&hash);
+        }
+        chunk_count += 1;
+    }
+
+    Ok(if chunk_count > 1 {
+        format!("{}-{}", hex_encode(&concat_hasher.finalize()), chunk_count)
+    } else if chunk_count == 1 {
+        hex_encode(&first_hash)
+    } else {
+        String::new()
+    })
 }
 
 #[allow(dead_code)]
